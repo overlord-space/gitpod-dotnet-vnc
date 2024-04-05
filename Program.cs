2e2a8fe6cfa4495254042d07e2dd8a109c64220f
overlord.space <7531551+overlord-space@users.noreply.github.com>
2024-04-05 12:27:08 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+// RUN `dotnet add package Selenium.WebDriver` to install Selenium csharp library 
+// and uncomemnt lines to run Seleinum WebDriver
+// using OpenQA.Selenium;
+// using OpenQA.Selenium.Chrome;
 
 namespace dotnetcore
 {
@@ -6,7 +10,12 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
+            // IWebDriver driver = new ChromeDriver();
+            // driver.Navigate().GoToUrl("https://google.com");
+            
             Console.WriteLine("Hello World!");
+
+            // driver.Quit();
         }
     }
 }
